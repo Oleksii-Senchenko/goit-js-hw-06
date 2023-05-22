44a5928997457270711982013418bfadc0e6e37d
Oleksii-Senchenko <106326629+Oleksii-Senchenko@users.noreply.github.com>
2023-05-23 00:51:16 +0300

    fix output to hex

--- js/task-09.js
+++ js/task-09.js
@@ -3,10 +3,10 @@ function getRandomHexColor() {
     .toString(16)
     .padStart(6, 0)}`;
 }
-const bodyEl = document.querySelector('body');
-const btnEl = document.querySelector('.change-color');
-const textEl = document.querySelector('.color');
-btnEl.addEventListener('click', () => {
+const bodyEl = document.querySelector("body");
+const btnEl = document.querySelector(".change-color");
+const textEl = document.querySelector(".color");
+btnEl.addEventListener("click", () => {
   bodyEl.style.backgroundColor = getRandomHexColor();
-  textEl.textContent = bodyEl.style.backgroundColor;
+  textEl.textContent = getRandomHexColor();
 });
